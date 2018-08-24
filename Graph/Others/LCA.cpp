3fc2968551835ae7ac7a4bb01c53d1b28d37285b
wcysai <wcysai@foxmail.com>
2018-08-25 01:51:56 +0800

    modified LCA

--- Graph/Others/LCA.cpp
+++ Graph/Others/LCA.cpp
@@ -3,12 +3,12 @@
 #define MAXLOGN 20
 using namespace std;
 vector<int> G[MAXN];
-int parent[MAXLOGN][MAXN];
+int pa[MAXLOGN][MAXN];
 int depth[MAXN];
 int n,q;
 void dfs(int v,int p,int d)
 {
-    parent[0][v]=p;
+    pa[0][v]=p;
     depth[v]=d;
     for(int i=0;i<(int)G[v].size();i++)
         if(G[v][i]!=p) dfs(G[v][i],v,d+1);
@@ -20,29 +20,32 @@ void init(int V)
     {
         for(int v=1;v<=V;v++)
         {
-            if(parent[k][v]<0) parent[k+1][v]=-1;
-            else parent[k+1][v]=parent[k][parent[k][v]];
+            if(pa[k][v]<0) pa[k+1][v]=-1;
+            else pa[k+1][v]=pa[k][pa[k][v]];
         }
     }
+}
+int get(int v,int x)
+{
+    for(int k=0;k<MAXLOGN;k++)
+        if((x>>k)&1)
+            v=pa[k][v];
+    return v;
 }
 int lca(int u,int v)
 {
     if(depth[u]>depth[v]) swap(u,v);
-    for(int k=0;k<MAXLOGN;k++)
-    {
-        if((depth[v]-depth[u])>>k&1)
-            v=parent[k][v];
-    }
+    v=get(v,depth[v]-depth[u]);
     if(u==v) return u;
     for(int k=MAXLOGN-1;k>=0;k--)
     {
-        if(parent[k][u]!=parent[k][v])
+        if(pa[k][u]!=pa[k][v])
         {
-            u=parent[k][u];
-            v=parent[k][v];
+            u=pa[k][u];
+            v=pa[k][v];
         }
     }
-    return parent[0][u];
+    return pa[0][u];
 }
 int dis(int u,int v)
 {

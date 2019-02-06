368fc76fb2351961303da6f8f0d99d8e32d87f3a
wcysai <wcysai@foxmail.com>
2019-02-06 17:53:00 +0800

    Modified KuhnMunkres

--- Graph/Matching algorithms/KuhnMunkres.cpp
+++ Graph/Matching algorithms/KuhnMunkres.cpp
@@ -9,40 +9,41 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> P;
 int n;
-int a[MAXN][MAXN],p[MAXN],minv[MAXN],way[MAXN];
-ll u[MAXN],v[MAXN];
-int km()
+int A[MAXN],B[MAXN],K[MAXN];
+int w[MAXN][MAXN];
+//minimum weight bipartite matching
+ll km(int n,int m)
 {
-    long long res=0;
-	for(int i=1;i<=n;i++)
+    vector<int> u(n+1),v(m+1),p(m+1),way(m+1);
+    for(int i=1;i<=n;i++) 
     {
-		p[0]=i;
-		int j0=0;
-		vector<ll> minv(n+1,INF);
-		vector<char> used(n+1,false);
-		do
+        p[0]=i;
+        int j0=0;
+        vector<int> minv(m+1,INF);
+        vector<char> used(m+1,false);
+        do 
         {
-			used[j0]=true;
-			int i0=p[j0],j1;
-			long long delta=INF;
-			for(int j=1;j<=n;j++)
-				if(!used[j])
+            used[j0]=true;
+            int i0=p[j0],delta=INF,j1;
+            for(int j=1;j<=m;++j)
+                if(!used[j]) 
                 {
-					ll cur=a[i0][j]-u[i0]-v[j];
-					if(cur<minv[j]) minv[j]=cur,way[j]=j0;
-					if(minv[j]<delta) delta=minv[j],j1=j;
-				}
-			for(int j=0;j<=n;j++) if(used[j]) u[p[j]]+=delta,v[j]-=delta; else minv[j]-=delta;
-			j0=j1;
-		}while (p[j0]!=0);
-		do 
+                    int cur=w[i0][j]-u[i0]-v[j];
+                    if(cur<minv[j]) minv[j]=cur,way[j]=j0;
+                    if(minv[j]<delta) delta=minv[j],j1=j;
+                }
+            for(int j=0;j<=m;++j) if(used[j]) u[p[j]]+=delta,v[j]-=delta; else minv[j]-=delta;
+            j0=j1;
+        }while(p[j0]!=0);
+        do 
         {
-			int j1=way[j0];
-			p[j0]=p[j1];
-			j0=j1;
-		}while(j0);
-		res=max(res,v[0]);
-	}
+            int j1=way[j0];
+            p[j0]=p[j1];
+            j0=j1;
+        }while(j0);
+    }
+    ll res=0;
+    for(int i=1;i<=m;i++) res+=w[p[i]][i];
     return res;
 }
 int main()

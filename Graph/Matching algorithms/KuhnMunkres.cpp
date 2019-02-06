ddfe62c3ba52612e7310741f31a2b4a1a24030e1
wcysai <wcysai@foxmail.com>
2019-02-06 17:25:46 +0800

    Modified KuhnMunkres

--- Graph/Matching algorithms/KuhnMunkres.cpp
+++ Graph/Matching algorithms/KuhnMunkres.cpp
@@ -1,112 +1,52 @@
+#pragma GCC optimize(3)
 #include<bits/stdc++.h>
-#define MAXN 505
+#define MAXN 1005
 #define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
 using namespace std;
-int w[MAXN][MAXN],x[MAXN],y[MAXN];
-int prev_x[MAXN],prev_y[MAXN],son_y[MAXN],slack[MAXN],par[MAXN];
-int lx,ly,pop;
-void adjust(int v)
-{
-    son_y[v]=prev_y[v];
-    if(prev_x[son_y[v]]!=2)
-        adjust(prev_x[son_y[v]]);
-}
-bool find(int v)
-{
-    for(int i=0;i<pop;i++)
-    {
-        if(prev_y[i]==-1)
-        {
-            if(slack[i]>x[v]+y[i]-w[v][i])
-            {
-                slack[i]=x[v]+y[i]-w[v][i];
-                par[i]=v;
-            }
-            if(x[v]+y[i]==w[v][i])
-            {
-                prev_y[i]=v;
-                if(son_y[i]==-1)
-                {
-                    adjust(i);
-                    return true;
-                }
-                if(prev_x[son_y[i]]!=-1)
-                    continue;
-                prev_x[son_y[i]]=i;
-                if(find(son_y[i]))
-                    return true;
-            }
-        }
-    }
-    return false;
-}
+typedef long long ll;
+typedef pair<int,int> P;
+int n;
+int a[MAXN][MAXN],p[MAXN],minv[MAXN],way[MAXN];
+ll u[MAXN],v[MAXN];
 int km()
 {
-    int m;
-    for(int i=0;i<pop;i++)
+    long long res=0;
+	for(int i=1;i<=n;i++)
     {
-        son_y[i]=-1;
-        y[i]=0;
-    }
-    for(int i=0;i<pop;i++)
-    {
-        x[i]=0;
-        for(int j=0;j<pop;j++)
-            x[i]=max(x[i],w[i][j]);
-    }
-    bool flag;
-    for(int i=0;i<pop;i++)
-    {
-        for(int j=0;j<pop;j++)
+		p[0]=i;
+		int j0=0;
+		vector<ll> minv(n+1,INF);
+		vector<char> used(n+1,false);
+		do
         {
-            prev_x[j]=prev_y[j]=-1;
-            slack[j]=INF;
-        }
-        prev_x[i]=-2;
-        if(find(i)) continue;
-        flag=false;
-        while(!flag)
-        {
-            m=INF;
-            for(int j=0;j<pop;j++)
-                if(prev_y[j]==-1)
-                    m=min(m,slack[j]);
-            for(int j=0;j<pop;j++)
-            {
-                if(prev_x[j]!=-1)
-                    x[j]-=m;
-                if(prev_y[j]!=-1)
-                    y[j]+=m;
-                else
-                    slack[j]-=m;
-            }
-            for(int j=0;j<pop;j++)
-            {
-                if(prev_y[j]==-1&&!slack[j])
+			used[j0]=true;
+			int i0=p[j0],j1;
+			long long delta=INF;
+			for(int j=1;j<=n;j++)
+				if(!used[j])
                 {
-                    prev_y[j]=par[j];
-                    if(son_y[j]==-1)
-                    {
-                        adjust(j);
-                        flag=true;
-                        break;
-                    }
-                    prev_x[son_y[j]]=j;
-                    if(find(son_y[j]))
-                    {
-                        flag=true;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    int ans=0;
-    for(int i=0;i<pop;i++)
-        ans+=w[son_y[i]][i];
-    return ans;
+					ll cur=a[i0][j]-u[i0]-v[j];
+					if(cur<minv[j]) minv[j]=cur,way[j]=j0;
+					if(minv[j]<delta) delta=minv[j],j1=j;
+				}
+			for(int j=0;j<=n;j++) if(used[j]) u[p[j]]+=delta,v[j]-=delta; else minv[j]-=delta;
+			j0=j1;
+		}while (p[j0]!=0);
+		do 
+        {
+			int j1=way[j0];
+			p[j0]=p[j1];
+			j0=j1;
+		}while(j0);
+		res=max(res,v[0]);
+	}
+    return res;
 }
 int main()
 {
     return 0;
 }
+

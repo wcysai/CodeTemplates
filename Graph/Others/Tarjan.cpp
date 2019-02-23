2da8d31cc3f10e774b17e471971fa8c3e2dae92c
wcysai <wcysai@foxmail.com>
2019-02-23 23:57:21 +0800

    Added Tarjan's Connected Component Algorithm

---
+++ Graph/Others/Tarjan.cpp
@@ -0,0 +1,52 @@
+#pragma GCC optimize(3)
+#include<bits/stdc++.h>
+#define MAXN 100005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+vector<int> G[MAXN];
+int n,dfn[MAXN],low[MAXN],st[MAXN];
+int vis[MAXN];
+int cmp[MAXN],cnt,tot,t;
+void dfs(int v)
+{
+    dfn[v]=low[v]=++tot;
+    vis[v]=1;
+    st[t++]=v;
+    for(auto to:G[v])
+    {
+        if(!vis[to])
+        {
+            dfs(to);
+            low[v]=min(low[v],low[to]);
+        }
+        else if(vis[to]==1) low[v]=min(low[v],dfn[to]);
+    }
+    if(dfn[v]==low[v])
+    {
+        int u;
+        do
+        {
+            u=st[t-1]; t--;
+            cmp[u]=cnt;
+            vis[u]=2;
+        }while(u!=v);
+        cnt++;
+    }
+}
+int tarjan()
+{
+    t=tot=cnt=0;
+    memset(vis,0,sizeof(vis));
+    for(int i=1;i<=n;i++) if(!dfn[i]) dfs(i);
+    return cnt;
+}
+int main()
+{
+    return 0;
+}
+

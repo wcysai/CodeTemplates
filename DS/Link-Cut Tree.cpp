938d4c6dd1854424caa181fd386cb4d1f9e01e21
wcysai <wcysai@foxmail.com>
2019-03-03 13:44:03 +0800

    modified implementation of link-cut tree

--- DS/Link-Cut Tree.cpp
+++ DS/Link-Cut Tree.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define MAXN 100005
+#define MAXN 300005
 #define INF 1000000000
 #define MOD 1000000007
 #define F first
@@ -13,52 +13,67 @@ typedef pair<int,int> P;
 typedef long long ll;
 namespace lct 
 {
-    struct meow{int ch[2], fa, rev, sum, w;} t[MAXN];
+    struct meow{int ch[2], fa, rev, sum, w;} t[2*MAXN];
     inline int wh(int x) {return t[pa].ch[1] == x;}
     inline int isr(int x) {return t[pa].ch[0] != x && t[pa].ch[1] != x;}
     inline void update(int x) {t[x].sum = t[lc].sum ^ t[rc].sum ^ t[x].w;}
     inline void rever(int x) {t[x].rev ^= 1; swap(lc, rc);}
     inline void pushdown(int x) 
     {
-        if(t[x].rev) {
+        if(t[x].rev) 
+        {
             if(lc) rever(lc);
             if(rc) rever(rc);
             t[x].rev = 0;
         }
     }
     void pd(int x) {if(!isr(x)) pd(pa); pushdown(x);}
-    inline void rotate(int x) {
+    inline void rotate(int x) 
+    {
         int f=t[x].fa, g=t[f].fa, c=wh(x);
-        if(!isr(f)) t[g].ch[wh(f)]=x; t[x].fa=g;
+        if(!isr(f)) t[g].ch[wh(f)]=x; 
+        t[x].fa=g;
         t[f].ch[c] = t[x].ch[c^1]; t[t[f].ch[c]].fa=f;
         t[x].ch[c^1] = f; t[f].fa=x;
         update(f); update(x);
     }
-    inline void splay(int x) {
+    inline void splay(int x) 
+    {
         pd(x);
         for(; !isr(x); rotate(x))
             if(!isr(pa)) rotate( wh(pa)==wh(x) ? pa : x );
     }
 
-    inline void access(int x) {
+    inline void access(int x) 
+    {
         for(int y=0; x; y=x, x=pa) splay(x), rc=y, update(x);
     }
-    inline void maker(int x) {
+    inline void maker(int x) 
+    {
         access(x); splay(x); rever(x);
     }
     inline int findr(int x) 
     {
         access(x); splay(x);
-        while(lc) pushdown(x), x=lc; return x;
+        while(lc) pushdown(x), x=lc; 
+        return x;
     }
-    inline void link(int x, int y) {
-        maker(x); t[x].fa=y;
+    inline void link(int x, int y) 
+    {
+        maker(x); 
+        if(findr(y)!=x) t[x].fa=y;
     }
-    inline void cut(int x, int y) {
-        maker(x); access(y); splay(y);
-        t[x].fa = t[y].ch[0] = 0; update(y);
+    inline void cut(int x, int y) 
+    {
+        maker(x);
+        if(findr(y)==x&&t[x].fa==y&&t[y].ch[0]==x&&!t[y].ch[1])
+        {
+            t[x].fa=t[y].ch[0]=0; 
+            update(y);
+        }
     }
-    inline void split(int x, int y) {
+    inline void split(int x, int y) 
+    {
         maker(x); access(y); splay(y);
     }
 } using lct::findr;
@@ -72,8 +87,8 @@ int main()
     {
         scanf("%d%d%d",&op,&x,&y);
         if(op==0) lct::split(x, y), printf("%d\n", lct::t[y].sum);
-        if(op==1) if(findr(x) != findr(y)) lct::link(x, y);
-        if(op==2) if(findr(x) == findr(y)) lct::cut(x, y);
-        if(op==3) lct::t[x].w = y, lct::splay(x);
+        if(op==1) lct::link(x, y);
+        if(op==2) lct::cut(x, y);
+        if(op==3) lct::t[x].w=y,lct::splay(x);
     }
 }

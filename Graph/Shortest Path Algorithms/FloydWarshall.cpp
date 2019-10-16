4fb235e09a03a374036d628a181ac313d19c34a6
wcysai <wcysai@foxmail.com>
2019-10-17 02:05:36 +0800

    Update FloydWarshall.cpp

--- Graph/Shortest Path Algorithms/FloydWarshall.cpp
+++ Graph/Shortest Path Algorithms/FloydWarshall.cpp
@@ -1,17 +1,10 @@
 #include<bits/stdc++.h>
-#define MAXV 10000
-#define MAXE 1000
-#define INF 1000000
+#define MAXN 505
 using namespace std;
-int d[MAXV][MAXV];
-int V;
+int n,d[MAXN][MAXN];
 void floyd_warshall()
 {
-    for(int k=0;k<V;k++)
-        for(int i=0;i<V;i++)
-          for(int j=0;j<V;j++) d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
-}
-int main()
-{
-    return 0;
+    for(int k=1;k<=n;k++)
+        for(int i=1;i<=n;i++)
+          for(int j=1;j<=n;j++) d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
 }

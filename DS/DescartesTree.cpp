a3e8f84c7f4c6d95b6679288e24451eb1ca7e3a4
wcysai <wcysai@foxmail.com>
2019-11-11 21:32:17 +0800

    Create DescartesTree.cpp

---
+++ DS/DescartesTree.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define MAXN 100005
+#define INF 1000000000
+#define MOD 1000000007
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+int n,h[MAXN];
+int st[MAXN],t;
+int fa[MAXN],ls[MAXN],rs[MAXN],root;
+int main()
+{
+    for(int i=1;i<=n;i++) scanf("%d",&h[i]);
+    for(int i=1;i<=n;i++)
+    {
+        while(t&&h[st[t-1]]>h[i]) ls[i]=st[--t];
+        if(t) rs[st[t-1]]=i;
+        st[t++]=i;
+    }
+    for(int i=1;i<=n;i++) fa[ls[i]]=fa[rs[i]]=i;
+    for(int i=1;i<=n;i++) if(!fa[i]) root=i;
+}

6c8d8b1c6ffb7e2cec87ef17f7573fa001d60ac9
Chunyang Wang <wcysai@foxmail.com>
2020-12-26 01:21:01 +0800

    Update PollardRho.cpp

--- Math/PollardRho.cpp
+++ Math/PollardRho.cpp
@@ -1,33 +1,100 @@
-#pragma GCC optimize(3)
 #include<bits/stdc++.h>
-#define MAXN 100005
+#define MAXN 1005
 #define INF 1000000000
 #define MOD 1000000007
 #define F first
 #define S second
 using namespace std;
-typedef long long ll;
-typedef pair<int,int> P;
-int n,k,a[MAXN];
-ll pollard_rho(ll n, int a)
+typedef unsigned long long ULL;
+//to achieve best running time, sieve until 2/3 prime factors remaining
+namespace pollardrho 
 {
-    ll x = 2, y = 2, d = 1, k = 0, i = 1;
-    while(d == 1)
-    {
-        ++k;
-        x = mul_mod(x, x, n) + a;
-        d = __gcd(x >= y ? x - y : y - x, n);
-        if(k == i)
-        {
-            y = x;
-            i <<= 1;
-        }
-    }
-    if(d == n) return pollard_rho(n, a + 1);
-    return d;
-}
-int main()
-{
-    return 0;
-}
+    ULL gcd(ULL a, ULL b) {return b ? gcd(b, a % b) : a;}
 
+    ULL mulmod(ULL x,ULL y,ULL p)
+    {
+        ULL z=(long double)x/p*y;
+        ULL res=(ULL)x*y-(ULL)z*p;
+        return (res+p)%p;
+    }
+
+    ULL powmod(ULL b, ULL e, ULL m) 
+    {
+        ULL r = 1;
+        while (e) 
+        {
+            if (e & 1) r = mulmod(r, b, m);
+            b = mulmod(b, b, m);
+            e >>= 1;
+        }
+        return r;
+    }
+
+    bool test(ULL n)
+    {
+        if (n < 3) return n==2;
+        // ! The array a[] should be modified if the range of x changes.
+        static const ULL a[] = {2, 3, 5, 7, 11, 13, 17, 19, 23,  ULLONG_MAX};//works for 1e18
+        ULL r = 0, d = n-1, x;
+        while (~d & 1) d >>= 1, r++;
+        for (int i=0; a[i] < n; i++)
+        {
+            x = powmod(a[i], d, n);
+            if (x == 1 || x == n-1) goto next;
+            for(int i=0;i<r;i++)
+            {
+                x = mulmod(x, x, n);
+                if (x == n-1) goto next;
+            }
+            return false;
+            next:;
+        }
+        return true;
+    }
+
+    map<ULL, int> mp;
+    mt19937_64 gen(time(NULL));
+
+    void PollardRho(ULL n) 
+    {
+        ULL c, x, y, d;
+        while (n % 2 == 0) 
+        {
+            mp[2]++;
+            n /= 2;
+        }
+        if (n == 1) return;
+
+        if (test(n)) 
+        {
+            mp[n]++;
+            return;
+        }
+
+        d = n;
+        static int counter = 0;
+        while (d == n) 
+        {
+            x = y = 2;
+            d = 1;
+            c = gen() % (n - 1) + 1;
+            while (d == 1) 
+            {
+                counter++;
+                x = (mulmod(x, x, n) + c) % n;
+                y = (mulmod(y, y, n) + c) % n;
+                y = (mulmod(y, y, n) + c) % n;
+                d = gcd(x > y ? x - y : y - x, n);
+            }
+        }
+        PollardRho(d);
+        PollardRho(n / d);
+    }
+
+    void work(ULL n,int id) 
+    {
+        PollardRho(n);
+        for(auto p:mp) fact[id].push_back(p);
+        mp.clear();
+    }
+}
